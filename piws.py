41a9489d974b6819b3c0e138d43eaf1712d7f836
carp <8250820+capricorn@users.noreply.github.com>
2019-08-18 14:53:47 -0500

    Track orderbook changes and handle reconnection

--- piws.py
+++ piws.py
@@ -12,6 +12,89 @@ import predictit as pi
 TRADE_TYPE_BUY = 1
 TRADE_TYPE_SELL = 3
 
+class Orderbook():
+    # Maybe a named tuple would be better in this case
+    '''
+    class Order():
+        def __init__(self, price, quantity):
+            self.price = price
+            self.quantity = quantity
+    '''
+
+    def __init__(self, bids, asks):
+        self.buy_orders = { str(p) : q for p,q in bids }
+        self.sell_orders = { str(p) : q for p,q in asks }
+
+class OrderbookDelta():
+    def __init__(self, ob):
+        self.prev_book = ob
+        self.curr_book = ob
+
+    def update(self, book):
+        self.prev_book = self.curr_book
+        self.curr_book = book
+
+    # Basically, what are the trades that can occur?
+    # 1. Someone buys from a seller
+    # 2. Someone sells to a buyer
+    # Yes, these are different
+    def get_changes(self):
+        changes = []
+        # Here, someone sold to a buyer (that is, somebody successfully bought shares)
+        for level in self.prev_book.buy_orders:
+            # Here, a price level was bought out
+            if level not in self.curr_book.buy_orders:
+                changes.append(('bought', int(level), self.prev_book.buy_orders[level]))
+            elif level in self.curr_book.buy_orders and self.prev_book.buy_orders[level] != self.curr_book.buy_orders[level]:
+                changes.append(('bought', int(level), abs(self.prev_book.buy_orders[level] - self.curr_book.buy_orders[level])))
+
+        for level in self.prev_book.sell_orders:
+            if level not in self.curr_book.sell_orders:
+                changes.append(('sold', int(level), self.prev_book.sell_orders[level]))
+            elif level in self.curr_book.sell_orders and self.prev_book.sell_orders[level] != self.curr_book.sell_orders[level]:
+                changes.append(('sold', int(level), abs(self.prev_book.sell_orders[level] - self.curr_book.sell_orders[level])))
+        return changes
+
+def find_order_quantity(delta):
+    return delta.get_changes()
+    changes = delta.get_changes()
+    if changes == []:
+        return 'No change'
+    prices = list(map(lambda k: float(k[0]), changes))
+    quantity = sum(map(lambda k: int(k[1]), changes))
+
+    return (sum(prices) / len(prices), quantity)
+
+class MarketStatsEvent():
+    def __init__(self):
+        self.market_id = ''
+        self.status = ''
+        self.timestamp = ''
+        self.shares_traded = 0
+
+    def __str__(self):
+        return f'market id: {self.market_id}\n' \
+               f'status: {self.status}\n' \
+               f'timestamp: {self.timestamp}\n' \
+               f'shares traded: {self.shares_traded}\n'
+
+    class MarketStatsDecoder(json.JSONDecoder):
+        def __init__(self, *args, **kwargs):
+            self.stats_event = MarketStatsEvent()
+            json.JSONDecoder.__init__(self, object_hook=self.hook, *args, **kwargs)
+
+        def hook(self, data):
+            if 'MarketId' in data:
+                self.stats_event.market_id = str(data['MarketId'])
+            if 'Status' in data:
+                self.stats_event.status = data['Status']
+            if 'TimeStamp' in data:
+                self.stats_event.timestamp = data['TimeStamp']
+            if 'TotalSharesTraded' in data:
+                self.stats_event.shares_traded = data['TotalSharesTraded']
+
+            return self.stats_event
+
 class ContractStatsEvent():
     def __init__(self):
         self.best_no_price = 0
@@ -133,6 +216,8 @@ class PredictItWebSocket():
         self.contract = None
         self.req_count = 1
         self.contract_filter = None
+        self.market_filter = None
+        self.contracts = None
 
     def subscribe_contract_orderbook(self, contract_id):
         pass
@@ -158,11 +243,18 @@ class PredictItWebSocket():
 
         req = requests.Request('GET', 'https://www.predictit.org/signalr/connect', params=params).prepare()
         url = 'wss://' + req.url[8:]
-        async with websockets.connect(url) as ws:
-            while True:
+        ws = await websockets.connect(url, ping_interval=None)
+        while True:
+            try:
                 data = await ws.recv()
                 data = json.loads(data)
                 await self.queue.put(self._parse_status_feed(data))
+            except websockets.ConnectionClosed:
+                logging.warning('Lost connection to status feed.')
+                self.feeds.cancel()
+                break
+                #ws = await websockets.connect(url, ping_interval=None)
+                #logging.info('Reconnected to status feed!')
 
     def _parse_shares_traded_event(self, msg):
         event = SharesTradedEvent()
@@ -184,6 +276,19 @@ class PredictItWebSocket():
         await ws.send(msg(self.req_count))
         self.req_count += 1
 
+    async def _init_trade_feed(self, ws):
+        await ws.recv()
+        await self.send_trade_feed_message(ws, self._init_sdk_msg())
+        await self.send_trade_feed_message(ws, self._subscribe_market_stats_msg())
+        await self.send_trade_feed_message(ws, self._subscribe_contract_stats_msg())
+
+        if self.contract:
+            await self.send_trade_feed_message(ws, self._subscribe_contract_orderbook_msg(self.contract))
+        # Clean up later 
+        if self.contracts:
+            for c in self.contracts:
+                await self.send_trade_feed_message(ws, self._subscribe_contract_orderbook_msg(c))
+
     async def connect_trade_feed(self):
         params = {
             'v': 5,
@@ -194,37 +299,46 @@ class PredictItWebSocket():
             'Host': 's-usc1c-nss-203.firebaseio.com'
         }
 
-        req = requests.Request('GET', 'https://s-usc1c-nss-203.firebaseio.com/.ws', params=params).prepare()
+        req = requests.Request('GET', 'https://s-usc1c-nss-202.firebaseio.com/.ws', params=params).prepare()
         url = 'wss://' + req.url[8:]
 
-        async with websockets.connect(url) as ws:
-            await ws.recv()
-            await self.send_trade_feed_message(ws, self._init_sdk_msg())
-            await self.send_trade_feed_message(ws, self._subscribe_market_stats_msg())
-            await self.send_trade_feed_message(ws, self._subscribe_contract_stats_msg())
-            if self.contract:
-                await self.send_trade_feed_message(ws, self._subscribe_contract_orderbook_msg(self.contract))
-            
-            while True:
+        #async with websockets.connect(url, ping_interval=None) as ws:
+        ws = await websockets.connect(url, ping_interval=None)
+        await self._init_trade_feed(ws)
+        
+        while True:
+            try:
                 data = await ws.recv()
                 event = self._route_trade_data(data)
                 if self.contract_filter and type(event) == ContractStatsEvent and self.contract_filter(event.contract_id):
                     continue
+                if self.market_filter and type(event) == MarketStatsEvent and self.market_filter(event.market_id):
+                    continue
                 await self.queue.put(event)
+            except websockets.ConnectionClosed:
+                logging.warning('Lost connection to trade feed.')
+                self.feeds.cancel()
+                break
+                #ws = await websockets.connect(url, ping_interval=None)
+                #await self._init_trade_feed(ws)
+                #logging.info('Reconnected to trade feed!')
 
     def set_queue_callback(self, callback):
         self.queue_callback = callback
 
     async def ping(self):
         while True:
-            await asyncio.sleep(60 * 5)
             self.start_time += 1
             #logging.info(f'Sending ping: {self.start_time}')
+            print(f'Sending ping: {self.start_time}')
             params = {
                 'bearer': self.p.token,
                 '_': self.start_time
             }
-            requests.get('https://www.predictit.org/signalr/ping', params=params)
+            resp = requests.get('https://www.predictit.org/signalr/ping', params=params)
+            #logging.info(f'ping response: {resp.status_code}')
+            print(f'ping response: {resp.status_code}')
+            await asyncio.sleep(60 * 5)
 
     async def _run_queue(self):
         while True:
@@ -268,19 +382,29 @@ class PredictItWebSocket():
         self.queue = asyncio.Queue()
         self.feeds = asyncio.gather(self.connect_trade_feed(), self.connect_status_feed(), 
                 self._run_queue(), self.ping())
-        await self.feeds
+        try:
+            await self.feeds
+            logging.info('Done with feeds')
+        except asyncio.CancelledError:
+            logging.info('Lost feeds, resetting')
+            time.sleep(1)
 
     def _route_trade_data(self, data):
         data = json.loads(data)
         if data == {}: return {}
-        if 'p' in data['d']['b']:
+        if 'b' in data['d'] and 'p' in data['d']['b']:
             msg = data['d']['b']['p']
             if msg.startswith('contractOrderBook'):
                 event = json.loads(str(data).replace("'", '"') , cls=OrderbookEvent.OrderbookEventDecoder)
                 return event
             if msg.startswith('contractStats'):
                 return json.loads(str(data).replace("'", '"'), cls=ContractStatsEvent.ContractStatsDecoder)
+            if msg.startswith('marketStats'):
+                return json.loads(str(data).replace("'", '"'), cls=MarketStatsEvent.MarketStatsDecoder)
         return data
+
+    def set_market_filter(self, mfilter):
+        self.market_filter = mfilter
 
     def stop(self):
         self.feeds.cancel()
